5fd55ea15ec2ef59110db43c2196169fcdc2cfe8
rsidhu-dataeko <r.sidhu@dataeko.ai>
2026-08-06 10:56:36 +0530

    Change password hashing method to pbkdf2:sha256

--- backend/app/auth/password.py
+++ backend/app/auth/password.py
@@ -7,7 +7,7 @@ from werkzeug.security import (
 def hash_password(password: str) -> str:
     return generate_password_hash(
         password,
-        method="scrypt",
+        method="pbkdf2:sha256",
     )
 
 
